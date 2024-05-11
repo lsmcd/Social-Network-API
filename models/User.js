1073bf0810148264af488c999c3dd85f624963c4
Logan McDaniel <lsmcd@pm.me>
2024-05-10 21:16:45 -0600

    add user model

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const {Schema, model} = require("mongoose");
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trimmed: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        set: toLower,
+        validate: (val) => {
+            val.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)
+        }
+        //https://regexr.com/2rhq7
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+});
+
+userSchema.virtual("friendCount").get(function() {
+    return this.friends.length;
+});
+
+const User = model("user", userSchema)
+
+module.exports = User;
