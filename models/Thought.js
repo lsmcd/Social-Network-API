c5d208910025b2533629c41861a190a0849a9f61
Logan McDaniel <lsmcd@pm.me>
2024-05-11 18:51:20 -0600

    add reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,30 @@
 const { Schema, model } = require("mongoose");
 
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: new Schema.Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        validate: (val) => {
+            return val.length <= 280;
+        }
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date(),
+        get: (date) => {
+            return date.toLocaleDateString();
+        }
+    }
+});
+
 const thoughtSchema = new Schema({
     thoughtText: {
         type: String,
@@ -33,4 +58,6 @@ thoughtSchema.virtual("reactionCount").get(() => {
 
 const Thought = model("thought", thoughtSchema);
 
-module.exports = Thought;
+const Reaction = model("reaction", reactionSchema);
+
+module.exports = {Thought, Reaction};
