68637c3b0ad5a5e541047d13c1c2c0daf700d7b9
Logan McDaniel <lsmcd@pm.me>
2024-05-13 18:11:43 -0600

    add last thought routes

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -18,11 +18,14 @@ async function getThought(req, res){
 async function postThought(req, res){
     try {
         let error; 
-        await Thought.create(req.body).catch((err) => error = err);
+        let thought = await new Thought.create(req.body).catch((err) => error = err);
+        let user = await User.findOne({username: req.body.username});
+        user.thoughts[user.thoughts.length] = thought._id;
+        user.save();
         if (!error){
             res.status(200).json("Successfully created");
         } else {
-            res.status(400).json(error)
+            res.status(400).json(error);
         }
     } catch (err) {console.log(err)}
 }
@@ -34,7 +37,7 @@ async function putThought(req, res){
         if (!error){
             res.status(200).json("Successfully updated");
         } else {
-            res.status(400).json(error)
+            res.status(400).json(error);
         }
     } catch (err) {console.log(err)}
 }
@@ -46,9 +49,44 @@ async function deleteThought(req, res){
         if (!error){
             res.status(200).json("Successfully deleted");
         } else {
-            res.status(400).json(error)
+            res.status(400).json(error);
         }
     } catch (err) {console.log(err)}
 }
 
-module.exports = {getAllThoughts, postThought, getThought, putThought, deleteThought};
+async function postReaction(req, res){
+    try {
+        let error; 
+        let thought = await Thought.findOne({_id: req.params.id}).catch((err) => error = err);
+        thought.reactions[thought.reactions.length] = {
+            reactionBody: req.body.body,
+            username: req.body.username
+        };
+        thought.save();
+        if (!error){
+            res.status(200).json("Successfully posted");
+        } else {
+            res.status(400).json(error);
+        }
+    } catch (err) {console.log(err)}
+}
+
+async function deleteReaction(req, res){
+    try {
+        let error; 
+        let thought = await Thought.findOneAndUpdate({_id: req.params.id},{ $pullAll: {reactions: [{$with: {reactionId: req.body.reactionId}}]}});
+        for (let i = 0; i < thought.reactions.length; i++){
+            if (thought.reactions[i].reactionId == req.body.reactionId){
+                thought.reactions = thought.reactions.slice(i, i);
+            }
+        }
+        console.log(thought)
+        thought.save()
+        if (!error){
+            res.status(200).json("Successfully deleted");
+        } else {
+            res.status(400).json(error)
+        }
+    } catch (err) {console.log(err)}
+}
+module.exports = {getAllThoughts, postThought, getThought, putThought, deleteThought, postReaction, deleteReaction};
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,9 +1,11 @@
 const router = require("express").Router()
-const {getAllThoughts, postThought, getThought, putThought, deleteThought} = require("../../controllers/thoughtController")
+const {getAllThoughts, postThought, getThought, putThought, deleteThought, postReaction, deleteReaction} = require("../../controllers/thoughtController")
 
 router.route("/").get(getAllThoughts).post(postThought);
 
 router.route("/:id").get(getThought).put(putThought).delete(deleteThought);
+
+router.route("/:id/reactions").post(postReaction).delete(deleteReaction);
 
 router.use((req, res) => {
   return res.status(404).send("Nonexistent route");
